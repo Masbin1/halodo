e37c824c68c5bf5554db62d2791d1256391e4453
masbin <m.bintangkhatulistiwa@gmail.com>
2025-08-03 22:16:56 +0700

    balbala

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bahnschrift: ['"Bahnschrift"', 'sans-serif'],
+        migraBold: ['"Migra ExtraBold"', 'serif'],
+        migraLight: ['"Migra ExtraLight"', 'serif'],
+        sloop: ['"Sloop Script"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
