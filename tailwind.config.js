61dee8ff7ab69bc45ffd13bc11db3f18a9cb648f
masbin <m.bintangkhatulistiwa@gmail.com>
2025-08-04 09:26:09 +0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./*.html"],
   theme: {
     extend: {
       fontFamily: {
         moreSugar: ['MoreSugar', 'cursive'],
       },
+      aspectRatio: {
+        '16/9': '16 / 9',
+      },
+      height: {
+        'screen-16/9': 'calc(100vh * 9 / 16)',
+      },
+      maxHeight: {
+        'screen-16/9': 'calc(100vh * 9 / 16)',
+      },
+      spacing: {
+        '1/20': '5%',
+        '1/10': '10%',
+        '3/20': '15%',
+        '1/5': '20%',
+      }
     },
   },
   plugins: [],
